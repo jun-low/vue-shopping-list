4752124dffc229f8d76964c87e4325eb181f0026
jun-low <zhijun.low@gmail.com>
2020-10-31 15:18:50 +0800

    feat: add shopping list component and footer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <shopping-list msg="Welcome to my shopping list app"/>
+    <footer>
+      Illustration by Kathy Dong. Code and Design by <a href="https://jun-low.github.io/">Jun Low</a>. Made with 🛒 in M'sia.
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShoppingList from './components/ShoppingList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ShoppingList
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Syne&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Syne', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+footer{
+  border-top: 2px solid;
+  padding: 24px;
+  width: 50%;
+  margin: 10% auto 0 auto;
 }
 </style>
